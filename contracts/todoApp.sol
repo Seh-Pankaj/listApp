6f897c5ff00e29b830d4d25655e6ba7e6d19c74a
Seh-Pankaj <pankajsehrawat.605@gmail.com>
2022-08-15 13:19:56 +0530

    150822

--- contracts/todoApp.sol
+++ contracts/todoApp.sol
@@ -1,7 +1,25 @@
-pragma solidity >=0.5.0;
+pragma solidity ^0.5.0;
 
 contract toDoList {
 
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Welcome to the decentralized ToDo App");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 
 }
